8d03242d42c2e6f045f06599a23d279185b6ab76
Michyjones <mykembuguah11@gmail.com>
2018-04-04 00:11:08 +0300

    remove unnecesary code

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
 import os
-import os
-from app import flask_app
-
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    flask_app.run(host='0.0.0.0', port=port, debug=True)
-import os
 from app import flask_app
 
 
